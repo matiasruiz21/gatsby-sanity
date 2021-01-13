bcb760590362c2e1df08ce7e4be31426671f71c9
Matias Ruiz <matiasruiz6899@gmail.com>
2021-01-13 18:29:16 -0300

    remuve watch mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
         projectId: "mqodunvr",
         dataset: "production",
         graphqlTag: "default",
-        watchMode: "true"
       },
     },
     "gatsby-plugin-sass",
